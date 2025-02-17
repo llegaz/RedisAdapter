8e02bc0d43d4980bc988265e54c0d4ac56ed2735
Laurent Legaz <laurent@legaz.eu>
2025-02-17 10:54:40 +0100

    meh reimplementing the wheel again lmao

--- src/Logger/DebugLogger.php
+++ src/Logger/DebugLogger.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use Psr\Log\LogLevel;
 use Symfony\Component\VarDumper\VarDumper;
 use Throwable;
 
@@ -13,46 +12,6 @@ use Throwable;
  */
 class DebugLogger extends Psr\Log\AbstractLogger
 {
-    public function alert(string|\Stringable $message, mixed $context = []): void
-    {
-        $this->log(LogLevel::ALERT, $message, $context);
-    }
-
-    public function critical(string|\Stringable $message, mixed $context = []): void
-    {
-        $this->log(LogLevel::CRITICAL, $message, $context);
-    }
-
-    public function debug(string|\Stringable $message, mixed $context = []): void
-    {
-        $this->log(LogLevel::DEBUG, $message, $context);
-    }
-
-    public function emergency(string|\Stringable $message, mixed $context = []): void
-    {
-        $this->log(LogLevel::EMERGENCY, $message, $context);
-    }
-
-    public function error(string|\Stringable $message, mixed $context = []): void
-    {
-        $this->log(LogLevel::ERROR, $message, $context);
-    }
-
-    public function info(string|\Stringable $message, mixed $context = []): void
-    {
-        $this->log(LogLevel::INFO, $message, $context);
-    }
-
-    public function notice(string|\Stringable $message, mixed $context = []): void
-    {
-        $this->log(LogLevel::NOTICE, $message, $context);
-    }
-
-    public function warning(string|\Stringable $message, mixed $context = []): void
-    {
-        $this->log(LogLevel::WARNING, $message, $context);
-    }
-
     public function log($level, string|\Stringable $message, mixed $context = []): void
     {
         try {
